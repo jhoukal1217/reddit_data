87b9e54b362235fc4c28d34f8b3b6fdaf1c6d229
jhoukal1217 <35942230+jhoukal1217@users.noreply.github.com>
2022-07-11 12:46:45 -0500

    Create config.py

---
+++ lambda/config.py
@@ -0,0 +1,24 @@
+rdt_client_id = 'sauG3nZgYEF4yGXUhI-lig'
+rdt_client_secret = 'Tkrec0LHzl-1LR8nq_RZ488KCRZobg'
+rdt_user_agent = 'bi_scraper_dev'
+rdt_username = 'bi_scraper_dev'
+rdt_password = '5ae58fc810183e620f34dea0b1a5d034'
+
+rdt_subreddit = 'dataengineering'
+rdt_interval = "week"
+rdt_limit = 1000
+
+post_fields = ('id',
+               'title',
+               'score',
+               'num_comments',
+               'author',
+               'created_utc',
+               'url',
+               'ups',
+               'downs',
+               'upvote_ratio',
+               )
+
+aws_s3_bucket = 'reddit-api-data'
+
